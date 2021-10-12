746d5900fcaa7b04b694877593e76551d78fba25
unknown <giselaclaure@gmail.com>
2021-10-13 00:02:05 +0200

    iteracion_6

---
+++ iteracion_6.js
@@ -0,0 +1,31 @@
+//6.1
+const numbers = [32, 21, 63, 95, 100, 67, 43];
+const number100 = numbers.find(number => number === 100)
+console.log(number100)
+
+//6.2
+const movies = [
+	{title: 'Madagascar', stars: 4.5, date: 2015},
+	{title: 'Origen', stars: 5, date: 2010},
+	{title: 'Your Name', stars: 5, date: 2016}
+];
+const findMovie = movies.find(movie => movie.date === 2010)
+console.log(findMovie)
+//6.3
+
+const aliens = [
+	{name: 'Zalamero', planet: 'Eden', age: 4029},
+	{name: 'Paktu', planet: 'Andromeda', age: 32},
+	{name: 'Cucushumushu', planet: 'Marte', age: 503021}
+];
+const mutations = [
+	{name: 'Porompompero', description: 'Hace que el alien pueda adquirir la habilidad de tocar el tambor'},
+	{name: 'Fly me to the moon', description: 'Permite volar, solo y exclusivamente a la luna'},
+	{name: 'Andando que es gerundio', description: 'Invoca a un señor mayor como Personal Trainer'}
+];
+
+let AlienNames=aliens.find(element=>element.name=='Cucushumushu')
+let mutationNames=mutations.find(element=>element.name=='Porompompero')
+console.log(mutationNames)
+let concat={...AlienNames,mutationNames}
+console.log(concat)
